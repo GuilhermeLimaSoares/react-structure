3843980136112c6d1bb378ef93eb3bd01f13ede9
GuilhermeLimaSoares <guilhermelimasoares9@gmail.com>
2020-02-20 07:43:25 -0300

    Projeto com estrutura react organizada

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import App from './components/App';
+
+ReactDom.render(
+	//<h1>Olá Mundo!</h1>,
+	<App />,
+	document.getElementById("app")
+);
